6e3b4ee0e60ace77cc15b61b4b03cb1aa3c81cf4
waldyr06 <waldyrfelype2012@gmail.com>
2024-12-01 21:43:24 -0300

    Tela de esporte ta pronta, agora tem que por suas reportagens

--- app/routes.py
+++ app/routes.py
@@ -7,12 +7,24 @@ def init_app(app):
 
     @app.route('/report1')
     def report1_page():
-        return render_template('report1.html')
+        return render_template('preport1.html')
 
     @app.route('/report2')
     def report2_page():
-        return render_template('report2.html')
+        return render_template('preport2.html')
 
     @app.route('/report3')
     def report3_page():
-        return render_template('report3.html')
+        return render_template('preport3.html')
+    
+    @app.route('/esporte4')
+    def esporte_report4_page():
+        return render_template('ereport4.html')
+    
+    @app.route('/esporte5')
+    def esporte_report5_page():
+        return render_template('ereport5.html')
+    
+    @app.route('/esporte6')
+    def esporte_report6_page():
+        return render_template('ereport6.html')
