1c2a029a81cccb2dcb73ea23e168ae8723b53d54
Silvania Jeronimo <caiosilva10012@gmail.com>
2024-12-05 16:50:00 -0300

    Healthy Screen(Update - Full) + Menu Screen (Base)

--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,22 @@
 from flask import render_template
 
 def init_app(app):
+    @app.route('/main')
+    def menu_page():
+        return render_template('menu.html')
     @app.route('/saude')
-    def saude_page():
+    def healthy_page():
         return render_template('teladesaude.html')
     
-    @app.route('/report1')
-    def report1_page():
-        return render_template('report1.html')
+    @app.route('/reportsa1')
+    def reportsa1_page():
+        return render_template('reportsaude1.html')
 
-    @app.route('/report2')
-    def report2_page():
-        return render_template('report2.html')
+    @app.route('/reportsa2')
+    def reportsa2_page():
+        return render_template('reportsaude2.html')
 
-    @app.route('/report3')
-    def report3_page():
-        return render_template('report3.html')
+    @app.route('/reportsa3')
+    def reportsa3_page():
+        return render_template('reportsaude3.html')
+   
